d7423ef04b95542a254e606ea95d65afbfbfbb9d
yagmurbugday <z.y.bugday@gmail.com>
2024-10-22 14:02:26 +0200

    HB fitting changes applied

--- inputs.py
+++ inputs.py
@@ -1,3 +1,28 @@
+color_palette = plt.cm.tab20(np.linspace(0, 1, 20))
+markersize=10
+
+#Running the HB fitting for HB informed fitting
+#Option 1: Read a previous run case
+run_HB = 'no'
+readHB = 'HBfit_2024_10_22_13_50'
+
+#Option2: Run the HB fitting from scratch
+run_HB = 'yes'
+extract_HB_param_from_laos = 'yes' #use LAOS data?
+data_index_for_laos_built_flow_curves = [0,1,2,7,8,9,14,15,16]
+skip_first = 3 #skip the first * of laos extracted flow data
+
+extract_HB_param_from_stress_growth = 'yes' #use stress growth data?
+current_time = datetime.datetime.now()
+formatted_time = current_time.strftime('%Y_%m_%d_%H_%M')
+
+extract_HB_param_from_external = 'yes' #provide an external result?
+HB_external_params = [1000, 10, 0.2] #sigma_y, dot_gamma_cr, n
+error_HBext = [0,0,0] #sigma_y, dot_gamma_cr, n
+
+
+
+
 #running from scratch?
 run_all = 'yes' #yes or no
 
@@ -20,6 +45,14 @@ laos_amplitudes = []
 laos_frequencies = []
 
 
+
+#Stress Growth Data if exsits
+str_grw_shear_rates = [0.063, 0.1, 0.17, 0.28, 0.45, 0.73, 1.2,\
+                1.96, 3.21, 5.25, 8.58, 14,23,37,61]
+target_strain =120 # to extract the stress at
+
+
+
 #SAOS fitting inputs
 modulus_boundary = []
 trel_boundary = []
@@ -35,15 +68,8 @@ bounds_alph =  [1e-1, 8e-1]
 bounds_gdcr = [1e-2,1e3] 
 bounds_I1cr = [1e-4,1e3] 
 
-##do you also want to perform flow informed fitting?
-flow_informed = 'yes' #yes or no
 
-###Do you want to extract HB parameters from LAOS?
-extract_HB_param_from_laos = 'yes ' #yes or no
 
-###Do you want to provide external HB parameters?
-HB_external = 'yes'
-HB_external_params = ['sigy', 'n', 'dgcr']
 
 
 
